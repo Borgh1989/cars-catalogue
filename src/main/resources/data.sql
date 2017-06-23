4ae68a7de7ec98f58426efdc399352db23415217
Andrew Yurkevich <borgh1989@mail.ru>
2017-06-23 15:47:09 +0300

    new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO car(NAME, SPEED, VOLUME, TYPE) VALUES ('AUDI', 400, 1.8, 'sedan');
-INSERT INTO car(NAME, SPEED, VOLUME, TYPE) VALUES ('BMW', 350, 2.0, 'sedan');
+INSERT INTO car(NAME, SPEED, VOLUME, TYPE) VALUES ('audi', 400, 3.0, 'sedan');
+INSERT INTO car(NAME, SPEED, VOLUME, TYPE) VALUES ('bmw', 350, 2.0, 'sedan');
+INSERT INTO car(NAME, SPEED, VOLUME, TYPE) VALUES ('land rover', 380, 4.0, 'crossover');
+INSERT INTO car(NAME, SPEED, VOLUME, TYPE) VALUES ('volkswagen', 200, 1.6, 'minivan');
