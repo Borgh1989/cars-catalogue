d2209c477bc5f75a413bcd754b9f55bfc44df068
Andrew Yurkevich <borgh1989@mail.ru>
2017-06-27 14:10:48 +0300

    session cookie based SecurityConfig

--- src/main/java/com/andrewYurkevich/Configuration/SecurityConfig.java
+++ src/main/java/com/andrewYurkevich/Configuration/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.andrewYurkevich.Configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,5 +14,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 
-
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/css/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+    }
 }
